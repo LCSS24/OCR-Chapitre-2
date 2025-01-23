19bc6d9d9e99cad53f170a4caa880930a45c395f
Lucas C <lucas.carpentier24@gmail.com>
2025-01-23 11:15:24 +0100

    Refonte du code dans script.js pour une meilleure comprehension

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,39 +1,50 @@
 /* Récupération des catégories*/
 async function fetchCategories() {
-  const demande = await fetch("http://localhost:5678/api/categories");
-  const reponse = await demande.json();
-  return reponse;
+  try {
+
+    const demande = await fetch("http://localhost:5678/api/categories");
+    const reponse = await demande.json();
+    return reponse;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des catégories : " + error);
+  }
 }
 
 /* Récupération des travaux de Sophie Bluel*/
 async function fetchWork() {
-  const demande = await fetch("http://localhost:5678/api/works");
-  const reponse = await demande.json();
-  return reponse;
+  try {
+    const demande = await fetch("http://localhost:5678/api/works");
+    const reponse = await demande.json();
+    return reponse;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des travaux : " + error);
+  }
 }
 
 /* Ensuite, génération des éléments HTML des cards*/
-fetchWork().then((genFig) => {
-  for (let i = 0; i < genFig.length; i++) {
+function generateCards(items) {
+  const gallery = document.querySelector(".gallery");
+  // Création des éléments HTML
+  items.forEach((item) => {
     const figure = document.createElement("figure");
     const image = document.createElement("img");
     const texte = document.createElement("figcaption");
 
     /* Définition des contenus des balises*/
-    image.src = genFig[i].imageUrl;
-    image.alt = genFig[i].title;
-    texte.textContent = genFig[i].title;
+    image.src = item.imageUrl;
+    image.alt = item.title;
+    texte.textContent = item.title;
+    figure.id = item.categoryId;
 
     /* Affichage des cards*/
-    let gallery = document.querySelector(".gallery");
     gallery.appendChild(figure);
     figure.appendChild(image);
     figure.appendChild(texte);
-  }
-});
+  });
+}
 
 /* Génération des filtres de catégories*/
-fetchCategories().then((genCategories) => {
+function generateCategories(categories) {
   let portfolioh2 = document.querySelector("#portfolio h2");
 
   /* Création de la div "menu_filtres" et intégration dans le HTML*/
@@ -42,22 +53,35 @@ fetchCategories().then((genCategories) => {
   menu_filtres.classList.add("menu_filtres");
 
   /* Ajout du bouton de filtrage "Tous" */
-  const filtre_tous = document.createElement("a");
-  filtre_tous.href = "#";
+  const filtre_tous = document.createElement("button");
   filtre_tous.textContent = "Tous";
   menu_filtres.appendChild(filtre_tous);
 
   /* Génération des filtres*/
-  for (let i = 0; i < genCategories.length; i++) {
-    const filtre = document.createElement("a");
-    filtre.href = "#";
+  categories.forEach((categorie) => {
+    const filtre = document.createElement("button");
 
     /* Définition des contenus des balises*/
-    filtre.textContent = genCategories[i].name;
+    filtre.textContent = categorie.name;
+    filtre.id = categorie.id;
 
     /* Intégration des cards*/
     menu_filtres.appendChild(filtre);
-  }
-});
+  });
+}
 
-/* Fonction reliant les filtres à leurs catégories en backend*/
+function filterWorks(id, categoryId) {
+
+}
+
+
+/* Fonction main qui exécute toute les fonctions*/
+async function main() {
+  const works = await fetchWork();
+  const categories = await fetchCategories();
+  generateCards(works);
+  generateCategories(categories);
+}
+
+// Lancement de la fonction main au chargement de la page
+document.addEventListener("DOMContentLoaded", main);
