845821b5ce466bc3bb27c3a48b3cf52a80acb399
Lucas oui <lucas.carpentier24@gmail.com>
2025-02-26 10:38:50 +0100

    Ajout de commentaires + suppression des console.log

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,31 +1,30 @@
 async function formFetch() {
   const login = {
-    email :  document.getElementById("email").value,
-    password : document.getElementById("mdp").value
+    email: document.getElementById("email").value,
+    password: document.getElementById("mdp").value,
   };
-    const reponse = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify({
-        email : login.email,
-        password : login.password
-      })
-    })
-      if (!reponse.ok) {
-        const txterreur = document.getElementById("erreurmdp")
-        txterreur.style.display = "block";
-    } else{
-      const objetToken = await reponse.json();
-      sessionStorage.setItem("token",objetToken.token)
-      location = "index.html"
-    }
-}
-  
-  //
-  const form = document.querySelector("form")
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    formFetch(form);
+  const reponse = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: login.email,
+      password: login.password,
+    }),
   });
+  if (!reponse.ok) {
+    const txterreur = document.getElementById("erreurmdp");
+    txterreur.style.display = "block";
+  } else {
+    const objetToken = await reponse.json();
+    sessionStorage.setItem("token", objetToken.token);
+    location = "index.html";
+  }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  formFetch(form);
+});
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,3 @@
-
 /* Récupération des catégories*/
 async function fetchCategories() {
   try {
@@ -24,7 +23,7 @@ async function fetchWork() {
 /* Ensuite, génération des éléments HTML des cards*/
 function generateCards(items) {
   const gallery = document.querySelector(".gallery");
-  gallery.innerHTML = ""
+  gallery.innerHTML = "";
   // Création des éléments HTML
   items.forEach((item) => {
     const figure = document.createElement("figure");
@@ -76,20 +75,21 @@ function generateCategories(categories) {
 // Filtrage des travaux par catégorie
 function filterWorks() {
   const menu_filtres = document.querySelector(".menu_filtres");
-  const gallery = document.querySelector(".gallery")
+  const gallery = document.querySelector(".gallery");
   const figures = gallery.querySelectorAll("figure");
 
+  // Pour chaque boutons filtre
   menu_filtres.querySelectorAll("button").forEach((button) => {
+    //Ecouter le click
     button.addEventListener("click", () => {
+      //Récuper l'id du bouton clické
       const categoryId = button.id;
-      
-      figures.forEach(figure => {
+      //Et pour chaque figure, comparer l'id à celui du bouton
+      figures.forEach((figure) => {
         if (categoryId === "tous") {
           figure.style.display = "block";
         } else {
-          figure.style.display = figure.id === categoryId
-            ? "block" 
-            : "none";
+          figure.style.display = figure.id === categoryId ? "block" : "none";
         }
       });
     });
@@ -97,242 +97,252 @@ function filterWorks() {
 }
 
 // MODE ADMINISTRATEUR //
-async function fetchPhoto(photo,titre,categorie) {
-  const token = sessionStorage.getItem("token")
+async function fetchPhoto(photo, titre, categorie) {
+  const token = sessionStorage.getItem("token");
   const formData = new FormData();
-  formData.append("image", photo)
-  formData.append("title", titre)
-  formData.append("category", categorie)
+  formData.append("image", photo);
+  formData.append("title", titre);
+  formData.append("category", categorie);
   const reponse = await fetch("http://localhost:5678/api/works", {
     method: "POST",
     headers: {
-      "accept" : "application/json",
-      "Authorization" : 'Bearer ' + token
+      accept: "application/json",
+      Authorization: "Bearer " + token,
     },
-    body: formData
-  })
-  const data = await reponse.json()
-  console.log(data)
+    body: formData,
+  });
+  const data = await reponse.json();
 
   if (reponse.ok) {
-    const works = await fetchWork()
-    const categorie = await fetchCategories()
-    const modale2 = document.getElementById("modale2")
+    const works = await fetchWork();
+    const categorie = await fetchCategories();
+    const modale2 = document.getElementById("modale2");
 
-    genGalleryModale(works)
-    generateCards(works)
-    modale2.innerHTML = htmlmodale2
-    affichageModale2(categorie)
+    genGalleryModale(works);
+    generateCards(works);
+    modale2.innerHTML = htmlmodale2;
+    affichageModale2(categorie);
   }
 }
 
 // Fonction qui gère l'affichage des éléments disponibles seulement pour l'admin
 function affichageAdmin() {
-    document.querySelector(".modeedition").style.display = "flex"
-    document.querySelector(".menu_filtres").style.display = "none"
-    document.querySelector(".modifprojet").style.display = "block"
+  document.querySelector(".modeedition").style.display = "flex";
+  document.querySelector(".menu_filtres").style.display = "none";
+  document.querySelector(".modifprojet").style.display = "block";
 
-    // Changement du mot login en logout
-    const logintxt = document.querySelector("li:nth-child(3) a")
-    logintxt.textContent = "logout"
+  // Changement du mot login en logout
+  const logintxt = document.querySelector("li:nth-child(3) a");
+  logintxt.textContent = "logout";
 
-    // Deconnexion lors du logout
-    logintxt.addEventListener("click", (event) => {
-      event.preventDefault();
-      sessionStorage.removeItem("token");
-      window.location.href = "index.html"
-    })
+  // Deconnexion lors du logout
+  logintxt.addEventListener("click", (event) => {
+    event.preventDefault();
+    sessionStorage.removeItem("token");
+    window.location.href = "index.html";
+  });
 }
 
 function genGalleryModale(datas) {
   //Récupération de la galerie de la modale
-  const gallerymodale = document.querySelector(".gallerymodale")
-  gallerymodale.innerHTML = ""
+  const gallerymodale = document.querySelector(".gallerymodale");
+  gallerymodale.innerHTML = "";
   //Génération des images et attribution des id à chaque work
   datas.forEach((data) => {
-    const figure = document.createElement("figure")
-    const image = document.createElement("img")
-    const i = document.createElement("i")
- 
-    figure.classList.add("figimg")
-    figure.id = data.id
-    image.src = data.imageUrl
-    i.classList.add("fa-solid", "fa-trash-can")
-    i.dataset.poubelleid = data.id
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const i = document.createElement("i");
 
-    gallerymodale.appendChild(figure)
-    figure.appendChild(image)
-    figure.appendChild(i)
+    figure.classList.add("figimg");
+    figure.id = data.id;
+    image.src = data.imageUrl;
+    i.classList.add("fa-solid", "fa-trash-can");
+    i.dataset.poubelleid = data.id;
+
+    gallerymodale.appendChild(figure);
+    figure.appendChild(image);
+    figure.appendChild(i);
 
     //Au click sur la poubelle, stocker la valeur de l'id du work, la figure de la modale et celle de la section "mes projets"
     i.addEventListener("click", (e) => {
-      const elementclicked = e.target.dataset.poubelleid
-      const figure =e.target.closest("figure")
-      const figurepage = document.querySelector(`.gallery figure[data-pbid="${elementclicked}"]`)
+      const elementclicked = e.target.dataset.poubelleid;
+      const figure = e.target.closest("figure");
+      const figurepage = document.querySelector(
+        `.gallery figure[data-pbid="${elementclicked}"]`
+      );
 
       //Appel de la fonction de suppression avec comme arguments les 3 données ci-dessus
-      fetchDelete(elementclicked,figure,figurepage)
-    })
-  }
-)
-
+      fetchDelete(elementclicked, figure, figurepage);
+    });
+  });
 }
 
 function affichageModale1(travaux) {
-  const modalefond = document.querySelector(".modale_fond")
-  const modale = document.querySelector("modale")
-  const btnmodale1 = document.querySelector(".modeedition p")
-  const btnmodale2 = document.querySelector(".modifprojet")
-  const btnslist = [btnmodale1,btnmodale2]
-
+  const modalefond = document.querySelector(".modale_fond");
+  const modale = document.querySelector("modale");
+  const btnmodale1 = document.querySelector(".modeedition p");
+  const btnmodale2 = document.querySelector(".modifprojet");
+  const btnslist = [btnmodale1, btnmodale2];
 
   // Pour chaque bouton ("mode édition" et "modifier" a coté de Mes Projets), lors du click, afficher la modale
-  btnslist.forEach((bouton) => bouton.addEventListener("click", () => {
-    modalefond.style.display = "flex"
-  }))
+  btnslist.forEach((bouton) =>
+    bouton.addEventListener("click", () => {
+      modalefond.style.display = "flex";
+    })
+  );
 
   // Au click de la croix, la modale se ferme
-  document.querySelector(".croix i").addEventListener("click", () => modalefond.style.display = "none")
-  
+  document
+    .querySelector(".croix i")
+    .addEventListener("click", () => (modalefond.style.display = "none"));
+
   //Au click sur le fond, la modale se ferme
   modalefond.addEventListener("click", (e) => {
-    if (e.target === modalefond & e.target !== modale) {
-      modalefond.style.display = "none"
+    if ((e.target === modalefond) & (e.target !== modale)) {
+      modalefond.style.display = "none";
     }
-  })
+  });
 
   //Appel de la fonction de génération des travaux dans la gallery modale
-  genGalleryModale(travaux)
-
+  genGalleryModale(travaux);
 }
 // Fonction de suppression des figures
-async function fetchDelete(id,figmodale,figpage) {
-  const reponse = await fetch("http://localhost:5678/api/works/"+id, {
+async function fetchDelete(id, figmodale, figpage) {
+  const reponse = await fetch("http://localhost:5678/api/works/" + id, {
     method: "DELETE",
-    headers : {
-      "Accept" : "*/*",
-      "Authorization": `Bearer ${sessionStorage.getItem("token")}`
-      }
-  })
+    headers: {
+      Accept: "*/*",
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+  });
 
-  if (!reponse.ok) {
-    console.log("ca marche pas")
-} else {
-    figmodale.remove()
-    figpage.remove()
-}
+  if (reponse.ok) {
+    figmodale.remove();
+    figpage.remove();
+  }
 }
 
 function affichageModale2(categories) {
-  const boutonadd = document.getElementById("addphoto")
-  const modale1 = document.getElementById("modale1")
-  const modale2 = document.getElementById("modale2")
-  const btnretour = document.getElementById("arrowback")
-  const modalefond = document.querySelector(".modale_fond")
+  const boutonadd = document.getElementById("addphoto");
+  const modale1 = document.getElementById("modale1");
+  const modale2 = document.getElementById("modale2");
+  const btnretour = document.getElementById("arrowback");
+  const modalefond = document.querySelector(".modale_fond");
   // modale2.innerHTML = ""
   // Au click de la croix, la modale se ferme
-  document.getElementById("close").addEventListener("click", () => modalefond.style.display = "none")
+  document
+    .getElementById("close")
+    .addEventListener("click", () => (modalefond.style.display = "none"));
 
   btnretour.addEventListener("click", () => {
-    modale2.style.display = "none"
-    modale1.style.display = "flex"
-  })
+    modale2.style.display = "none";
+    modale1.style.display = "flex";
+  });
 
-  boutonadd.addEventListener("click",() => {
+  boutonadd.addEventListener("click", () => {
     // Modale 2 qui apparait
-    modale1.style.display = "none"
-    modale2.style.display = "flex"
-    })
+    modale1.style.display = "none";
+    modale2.style.display = "flex";
+  });
 
   categories.forEach((categorie) => {
-    const option = document.createElement("option")
-    const listeselection = document.getElementById("categories")
+    const option = document.createElement("option");
+    const listeselection = document.getElementById("categories");
 
-    option.value = categorie.id
-    option.text = categorie.name
+    option.value = categorie.id;
+    option.text = categorie.name;
 
-    listeselection.appendChild(option)
-
-    
-    })
-    ajouterPhoto()
-    formchecker()
+    listeselection.appendChild(option);
+  });
+  ajouterPhoto();
+  formchecker();
 }
 
 function ajouterPhoto() {
-  const inputfichier = document.getElementById("file")
+  const inputfichier = document.getElementById("file");
 
+  //Ecouter l'input, si un fichier est chargé...
   inputfichier.addEventListener("change", () => {
-  const image = inputfichier.files[0]
-  const imageURL = URL.createObjectURL(image)
-  const zoneadd = document.querySelector(".zoneadd")
-  const img = document.createElement("img")
-  const typeOk = ["image/jpeg", "image/png", "image/jpg"]
-  const erreurfile = document.querySelector(".zoneadd p")
+    //Récupération du fichier
+    const image = inputfichier.files[0];
+    //Création d'un url temporaire
+    const imageURL = URL.createObjectURL(image);
+    //Création de l'image et récupération de la zone où celle ci sera affichée
+    const zoneadd = document.querySelector(".zoneadd");
+    const img = document.createElement("img");
+    //Définition des formats valides
+    const typeOk = ["image/jpeg", "image/png", "image/jpg"];
+    //Récupération du message d'erreur
+    const erreurfile = document.querySelector(".zoneadd p");
 
-  if (image.size >= 4 * 1024 * 1024) {
-    erreurfile.textContent = "Le fichier est trop lourd (4mo)"
-    erreurfile.style.color = "rgb(255, 91, 91)"
-    zoneadd.appendChild(erreurfile)
-  } else if (!typeOk.includes(image.type)){
-    erreurfile.textContent = "Le type de fichier n'est pas valide (png,jpg,jpeg)"
-    erreurfile.style.color = "rgb(255, 91, 91)"
-  } else {
-    zoneadd.innerHTML = ""
-    img.src = imageURL
-    img.classList.add("imgload")
-    zoneadd.appendChild(img)
-    caca
-  }
-
-  })
+    //Si l'image n'est pas trop lourde (+ de 4mo)
+    if (image.size >= 4 * 1024 * 1024) {
+      erreurfile.textContent = "Le fichier est trop lourd (4mo)";
+      erreurfile.style.color = "rgb(255, 91, 91)";
+      zoneadd.appendChild(erreurfile);
+      //Si l'image n'est pas au format demandé
+    } else if (!typeOk.includes(image.type)) {
+      erreurfile.textContent =
+        "Le type de fichier n'est pas valide (png,jpg,jpeg)";
+      erreurfile.style.color = "rgb(255, 91, 91)";
+      //Vidage de la zoneadd et affichage de l'image
+    } else {
+      zoneadd.innerHTML = "";
+      img.src = imageURL;
+      img.classList.add("imgload");
+      zoneadd.appendChild(img);
+    }
+  });
 }
 
 function formchecker() {
-  const btnvalider = document.getElementById("valider")
-  const erreurform = document.querySelector(".erreurslctn")
-  const fileInput = document.getElementById("file")
-  const champTitre = document.getElementById("titre")
-  const champCategorie = document.getElementById("categories")
+  const btnvalider = document.getElementById("valider");
+  const erreurform = document.querySelector(".erreurslctn");
+  const fileInput = document.getElementById("file");
+  const champTitre = document.getElementById("titre");
+  const champCategorie = document.getElementById("categories");
 
+  //Fonction modifiant l'apparence du bouton d'envoi selon la complétion du formulaire
   function checkForm() {
-    const file = fileInput.files[0]
-    const titre = champTitre.value
-    const categorie = champCategorie.value
+    const file = fileInput.files[0];
+    const titre = champTitre.value;
+    const categorie = champCategorie.value;
 
-    const isFormValid = !!(file && titre.trim() && categorie !== "")
+    //Si le fichier est chargé, que le titre n'est pas vide et qu'une des catégorie est selectionnée alors true
+    const isFormValid = !!(file && titre.trim() && categorie !== "");
     if (isFormValid) {
       btnvalider.style.backgroundColor = "#1d6154";
-      erreurform.style.display = "none"
- 
+      erreurform.style.display = "none";
     } else {
       btnvalider.style.backgroundColor = "#a7a7a7";
-
     }
-    return isFormValid
+    return isFormValid;
   }
 
+  //Ecoute des champs du formulaire, à chaque changement, appelle la fonction de vérification
   champTitre.addEventListener("input", checkForm);
   champCategorie.addEventListener("change", checkForm);
   fileInput.addEventListener("change", checkForm);
-    
-  btnvalider.addEventListener("click",() => {
+
+  //Au click sur le bouton valider...
+  btnvalider.addEventListener("click", () => {
+    //Si true, alors envoi des données via la fonction fetchPhoto
     if (checkForm()) {
-      const photo = document.getElementById("file").files[0]
-      const titre = champTitre.value
-      const categorie = champCategorie.value
-      // Envoyer photo via fetch
-      fetchPhoto(photo,titre,categorie)
+      const photo = document.getElementById("file").files[0];
+      const titre = champTitre.value;
+      const categorie = champCategorie.value;
+      fetchPhoto(photo, titre, categorie);
+      //Sinon, message d'erreur
     } else {
       erreurform.style.display = "block";
     }
-  })
+  });
 }
 
 /* Fonction main qui exécute toute les fonctions*/
 async function main() {
   //Vide la modale 2, puis la copie au chargement de la page, utile pour recharger la modale lors de l'upload d'une photo
-  htmlmodale2 = ""
+  htmlmodale2 = "";
   htmlmodale2 = document.getElementById("modale2").innerHTML;
   const works = await fetchWork();
   const categories = await fetchCategories();
@@ -342,7 +352,7 @@ async function main() {
   if (sessionStorage.getItem("token")) {
     affichageAdmin();
     affichageModale1(works);
-    affichageModale2(categories)
+    affichageModale2(categories);
   }
 }
 
